e667817cfd09bdbe14dd55ca42c497d2f199d3c5
Andrey Volchkov <avolchkov@playtika.com>
2018-02-02 18:34:40 +0200

    first commit

---
+++ src/main/java/Task1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double priceCandy = scanner.nextDouble();
+    }
+}
